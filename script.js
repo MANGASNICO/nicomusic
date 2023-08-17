d8ce03de7818c424a4a9c2b1049062eecbf388c9
nicooo <128748897+MANGASNICO@users.noreply.github.com>
2023-08-17 17:05:02 -0300

    añado url a las cartas

--- script.js
+++ script.js
@@ -6,30 +6,35 @@ const listadoCartas = [
         nombre: "xxxtentacion",
         imagen: "./imagenes/xxxtentacion.jpeg",
         descripcion: "TOP 1: rip roach ",
+        url:"https://youtu.be/aFOxVm3Z9x8"
     },
     {
         id: 2,
         nombre: "litkillah",
         imagen: "./imagenes/litkilla.jpeg",
         descripcion: "TOP 2:bzrp freestyle session #3' ",
+        url:"https://youtu.be/ZgDFaniuwQc"
     },
     {
         id: 3,
         nombre: "travis scott",
         imagen: "./imagenes/travisscott.jpeg",
         descripcion: "TOP 3: who? what! ",
+        url:"https://youtu.be/gLkQA7iLNUk"
     },
     {
         id: 4,
         nombre: "duki",
         imagen: "./imagenes/duki.jpeg",
         descripcion: "TOP 4: she don't give a fo' ",
+        url:"https://youtu.be/W0yp3rSfx3I"
     },
     {
         id: 5,
         nombre: "Eminem",
         imagen: "./imagenes/eminem.jpeg",
         descripcion: "TOP 5: superman' ",
+        url:"https://youtu.be/8kYkciD9VjU"
     },
 ]
 /* Selección de los NODOS HTML (etiquetas/elementos html)*/
@@ -38,7 +43,9 @@ const contenedorCartas = document.querySelector(".listadoCartas")
 listadoCartas.forEach(
     (carta) => contenedorCartas.innerHTML += `
     <article class="card">
-        <img src=${carta.imagen} alt="Imagen del proyecto">
+        <a href=${carta.url}>
+            <img src=${carta.imagen} alt="Imagen del proyecto">
+        </a>
         <div class="info">
             <h2>${carta.nombre}</h2>
             <p>${carta.descripcion}</p>
